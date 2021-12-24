3addb42ee5dc6ed598c3e3754cfcdadc018709cf
Bryan Yehuda <bryanyehuda@gmail.com>
2021-12-24 11:20:22 +0700

    Fixing Fetching and Parsing JSON

--- app/src/main/java/com/its/dguard/DownloadFile.java
+++ app/src/main/java/com/its/dguard/DownloadFile.java
@@ -11,6 +11,12 @@ import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -26,6 +32,7 @@ public class DownloadFile extends AppCompatActivity {
     ImageButton download;
     String DOWNLOAD_URL = Endpoints.GET_FILE_URL;
     private ProgressDialog pDialog;
+    private RequestQueue mQueue;
     public static final int progress_bar_type = 0;
 
     @Override
@@ -33,30 +40,16 @@ public class DownloadFile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_download_file);
 
-        String jsonString = DOWNLOAD_URL ;
-        JSONObject obj = null;
-        try {
-            obj = new JSONObject(jsonString);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        String fileLink = null;
-        try {
-            fileLink = obj.getJSONObject("files").getString("file");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        download = (ImageButton) findViewById(R.id.downloadFile);
-        String finalFileLink = fileLink;
-        download.setOnClickListener(v -> new DownloadFileFromURL().execute(finalFileLink));
+        download = findViewById(R.id.downloadFile);
+        mQueue = Volley.newRequestQueue(this);
+        download.setOnClickListener(view -> jsonParse());
     }
 
     @Override
     protected Dialog onCreateDialog(int id) {
-        if (id == progress_bar_type) { // we set this to 0
+        if (id == progress_bar_type) {
             pDialog = new ProgressDialog(this);
-            pDialog.setMessage("Downloading file. Please wait...");
+            pDialog.setMessage("File sedang di download, mohon menunggu...");
             pDialog.setIndeterminate(false);
             pDialog.setMax(100);
             pDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
@@ -67,9 +60,25 @@ public class DownloadFile extends AppCompatActivity {
         return null;
     }
 
+    private void jsonParse() {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, DOWNLOAD_URL, null,
+                response -> {
+                    try {
+                        JSONArray jsonArray = response.getJSONArray("files");
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject link = jsonArray.getJSONObject(i);
+                            String file = link.getString("file");
+                            new DownloadFileFromURL().execute(file);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }, Throwable::printStackTrace);
+        mQueue.add(request);
+    }
 
     @SuppressLint("StaticFieldLeak")
-    class DownloadFileFromURL extends AsyncTask<String, String, String> {
+    class DownloadFileFromURL extends AsyncTask < String, String, String > {
 
         @Override
         protected void onPreExecute() {
@@ -78,7 +87,7 @@ public class DownloadFile extends AppCompatActivity {
         }
 
         @Override
-        protected String doInBackground(String... f_url) {
+        protected String doInBackground(String...f_url) {
             int count;
             try {
                 URL url = new URL(f_url[0]);
@@ -93,21 +102,19 @@ public class DownloadFile extends AppCompatActivity {
                 long total = 0;
                 while ((count = input.read(data)) != -1) {
                     total += count;
-                    publishProgress("" + (int) ((total * 100) / lenghtOfFile));
+                    publishProgress("" + (int)((total * 100) / lenghtOfFile));
                     output.write(data, 0, count);
                 }
-
                 output.flush();
                 output.close();
                 input.close();
-
             } catch (Exception e) {
                 Log.e("Error: ", e.getMessage());
             }
             return null;
         }
 
-        protected void onProgressUpdate(String... progress) {
+        protected void onProgressUpdate(String...progress) {
             pDialog.setProgress(Integer.parseInt(progress[0]));
         }
 
@@ -116,6 +123,5 @@ public class DownloadFile extends AppCompatActivity {
             dismissDialog(progress_bar_type);
         }
     }
-
 
 }
--- app/src/main/java/com/its/dguard/DownloadImg.java
+++ app/src/main/java/com/its/dguard/DownloadImg.java
@@ -6,69 +6,50 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.StrictMode;
 import android.util.Log;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class DownloadImg extends AppCompatActivity {
 
     ImageButton download;
-    String DOWNLOAD_URL = Endpoints.GET_FILE_URL;
+    String DOWNLOAD_URL = Endpoints.GET_PICS_URL;
     private ProgressDialog pDialog;
+    private RequestQueue mQueue;
     public static final int progress_bar_type = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_download_file);
+        setContentView(R.layout.activity_download_img);
 
-
-        download = (ImageButton) findViewById(R.id.downloadFile);
-        String result = null;
-        try {
-            result = HTTPGetCall(DOWNLOAD_URL);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        JSONObject obj = null;
-        try {
-            assert result != null;
-            obj = new JSONObject(result);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        String finalFileLink = null;
-        try {
-            assert obj != null;
-            finalFileLink = obj.getString("file");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        String finalFileLink1 = finalFileLink;
-        download.setOnClickListener(v -> new DownloadFileFromURL().execute(finalFileLink1));
+        download = findViewById(R.id.downloadImg);
+        mQueue = Volley.newRequestQueue(this);
+        download.setOnClickListener(view -> jsonParse());
     }
 
     @Override
     protected Dialog onCreateDialog(int id) {
         if (id == progress_bar_type) {
             pDialog = new ProgressDialog(this);
-            pDialog.setMessage("Downloading file. Please wait...");
+            pDialog.setMessage("File sedang di download, mohon menunggu...");
             pDialog.setIndeterminate(false);
             pDialog.setMax(100);
             pDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
@@ -79,27 +60,25 @@ public class DownloadImg extends AppCompatActivity {
         return null;
     }
 
-    protected String HTTPGetCall(String WebMethodURL) throws IOException {
-        StringBuilder response = new StringBuilder();
-        URL u = new URL(WebMethodURL);
-        HttpURLConnection conn = (HttpURLConnection) u.openConnection();
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        if (conn.getResponseCode() == HttpURLConnection.HTTP_OK)
-        {
-            BufferedReader input = new BufferedReader(new InputStreamReader(conn.getInputStream()),8192);
-            String line;
-            while ((line = input.readLine()) != null)
-            {
-                response.append(line);
-            }
-            input.close();
-        }
-        return response.toString();
+    private void jsonParse() {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, DOWNLOAD_URL, null,
+                response -> {
+                    try {
+                        JSONArray jsonArray = response.getJSONArray("images");
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject link = jsonArray.getJSONObject(i);
+                            String image = link.getString("image");
+                            new DownloadFileFromURL().execute(image);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }, Throwable::printStackTrace);
+        mQueue.add(request);
     }
 
     @SuppressLint("StaticFieldLeak")
-    class DownloadFileFromURL extends AsyncTask<String, String, String> {
+    class DownloadFileFromURL extends AsyncTask < String, String, String > {
 
         @Override
         protected void onPreExecute() {
@@ -108,7 +87,7 @@ public class DownloadImg extends AppCompatActivity {
         }
 
         @Override
-        protected String doInBackground(String... f_url) {
+        protected String doInBackground(String...f_url) {
             int count;
             try {
                 URL url = new URL(f_url[0]);
@@ -117,27 +96,25 @@ public class DownloadImg extends AppCompatActivity {
 
                 int lenghtOfFile = connection.getContentLength();
                 InputStream input = new BufferedInputStream(url.openStream(), 8192);
-                OutputStream output = new FileOutputStream(Environment.getExternalStorageDirectory().toString() + "/2011.pdf");
+                OutputStream output = new FileOutputStream(Environment.getExternalStorageDirectory().toString() + "/2021.png");
 
                 byte[] data = new byte[1024];
                 long total = 0;
                 while ((count = input.read(data)) != -1) {
                     total += count;
-                    publishProgress("" + (int) ((total * 100) / lenghtOfFile));
+                    publishProgress("" + (int)((total * 100) / lenghtOfFile));
                     output.write(data, 0, count);
                 }
-
                 output.flush();
                 output.close();
                 input.close();
-
             } catch (Exception e) {
                 Log.e("Error: ", e.getMessage());
             }
             return null;
         }
 
-        protected void onProgressUpdate(String... progress) {
+        protected void onProgressUpdate(String...progress) {
             pDialog.setProgress(Integer.parseInt(progress[0]));
         }
 
@@ -146,6 +123,5 @@ public class DownloadImg extends AppCompatActivity {
             dismissDialog(progress_bar_type);
         }
     }
-
 
 }
